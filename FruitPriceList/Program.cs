2e3efe55c444f7e3ba205e23346cf8bceb1ef4da
zeri <solev0025@gmail.com>
2026-02-27 01:03:51 +0900

    fianl

--- FruitPriceList/Program.cs
+++ FruitPriceList/Program.cs
@@ -1,6 +1,39 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Xml.Linq;
 
 // README.md를 읽고 코드를 작성하세요.
+
+//FruitPriceList[] list =
+//{
+//    new FruitPriceList { }, //[0]
+//    new FruitPriceList { }  //[1]
+//    new FruitPriceList {  }
+//};
+//list[0].PrintInfo(" ");
+//list[1].PrintInfo(" );
+//list[2].PrintInfo(" ");// 안됨.
+
+FruitPriceList list = new FruitPriceList(5);
+
+list.Add("사과", 1500);
+list.Add("바나나", 3000);
+list.Add("딸기", 8000);
+
+Console.WriteLine($"등록된 과일 수: {list.Count}개");
+
+Console.WriteLine($"사과 가격: {list["사과"]}원");
+Console.WriteLine($"바나나 가격: {list["바나나"]}원");
+Console.WriteLine($"포도 가격: {list["포도"]}원");
+
+for (int i = 0; i < list.Count; i++)
+    Console.WriteLine($"{i}번: {list[i]}");
+
+//foreach (var item in list)
+//{
+//    Console.WriteLine($"{}가격: {}");
+//} /사용 가능?
 class FruitPriceList
 {
     private string[] _names;
@@ -10,16 +43,38 @@ class FruitPriceList
     public FruitPriceList(int capacity)
     {
         _names = new string[capacity];
-        _count = 0;
+        _prices = new int[capacity];
     }
 
     public int Count
     {
-        get { return _names.Length; }
+        get { return _count; }
     }
     public void Add(string name, int price)
     {
-        _names = new string[name];
-        _prices = new int[price];
+        _names[_count] = name;
+        _prices[_count] = price;
+        _count++;
     }
+
+    public int this[string name]
+    {
+        get
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (_names[i] == name)
+                    return _prices[i];
+            }
+            return -1;
+        }
+    }
+    public string this[int index]
+    {
+        get
+        {
+            return _names[index];
+        }
+    }
+
 }
--- ScoreBoard/Program.cs
+++ ScoreBoard/Program.cs
@@ -3,37 +3,83 @@ using System.Reflection;
 
 // README.md를 읽고 코드를 작성하세요.
 
+ScoreBoard sb = new ScoreBoard(5);
+
+sb.Register("김민수", 85);
+sb.Register("이지영", 92);
+sb.Register("박서준", 78);
+
+Console.WriteLine($"등록된 학생 수: {sb.Count}명\n");
+
+for (int i = 0; i < sb.Count; i++)
+    Console.WriteLine($"{i}번: {sb[i]}");
+
+Console.WriteLine();
+Console.WriteLine($"김민수 점수: {sb["김민수"]}점");
+Console.WriteLine($"이지영 점수: {sb["이지영"]}점");
+Console.WriteLine($"홍길동 점수: {sb["홍길동"]}점");
+
+Console.WriteLine();
+sb["김민수"] = 95;   
+Console.WriteLine($"김민수 수정된 점수: {sb["김민수"]}점");
+
 class ScoreBoard
 {
     private string[] _names;
-    private int[] _score = new int[10];
+    private int[] _score;
     private int _count = 0;
 
     public ScoreBoard(int capacity)
     {
-        _names = new string[capacity]; 
-        Count = 0;
+        _names = new string[capacity];
+        _score = new int[capacity];
     }
-    public int Length
+    public int Count
     {
-        get { return _names.Length; }
+        get { return _count; }
     }
 
 
     public void Register(string name, int score)
     {
-        _names = name.Split(',');
-        _score = int[score];
+        if (_count >= _names.Length)
+        {
+            Console.WriteLine("성적표가 가득찼습니다");
+            return;
+        }
+        _names[_count] = name;
+        _score[_count] = score;
+        _count++;
     }
     public string this[int index]
     {
-        get { return _names[index]; }
-        set { _names[index] = value; }
+        get
+        {
+            return _names[index];
+        }
     }
 
     public int this[string name]
     {
-        get { return _names[name]; }
-        set { _names[name] = value; }
+        get
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (_names[i] == name)
+                    return _score[i];
+            }
+            return -1;
+        }
+        set
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (_names[i] == name)
+                {
+                    _score[i] = value;   
+                }
+            }
+        }
 
     }
+}
