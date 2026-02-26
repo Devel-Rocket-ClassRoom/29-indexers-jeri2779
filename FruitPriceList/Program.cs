560786e836c6abc7d6f773bc49478f2c394aa54f
jeri2779 <solev0025@gmail.com>
2026-02-26 18:07:02 +0900

    1st

--- FruitPriceList/Program.cs
+++ FruitPriceList/Program.cs
@@ -1,4 +1,25 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+class FruitPriceList
+{
+    private string[] _names;
+    private int[] _prices;
+    private int _count = 0;
+
+    public FruitPriceList(int capacity)
+    {
+        _names = new string[capacity];
+        _count = 0;
+    }
+
+    public int Count
+    {
+        get { return _names.Length; }
+    }
+    public void Add(string name, int price)
+    {
+        _names = new string[name];
+        _prices = new int[price];
+    }
+}
--- ScoreBoard/Program.cs
+++ ScoreBoard/Program.cs
@@ -1,4 +1,39 @@
 using System;
+using System.Reflection;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+class ScoreBoard
+{
+    private string[] _names;
+    private int[] _score = new int[10];
+    private int _count = 0;
+
+    public ScoreBoard(int capacity)
+    {
+        _names = new string[capacity]; 
+        Count = 0;
+    }
+    public int Length
+    {
+        get { return _names.Length; }
+    }
+
+
+    public void Register(string name, int score)
+    {
+        _names = name.Split(',');
+        _score = int[score];
+    }
+    public string this[int index]
+    {
+        get { return _names[index]; }
+        set { _names[index] = value; }
+    }
+
+    public int this[string name]
+    {
+        get { return _names[name]; }
+        set { _names[name] = value; }
+
+    }
